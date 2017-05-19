7335cbf4393ade32079a8a91ac341ddce569848c
GalRavil <gal.ravil93@gmail.com>
2017-05-19 20:48:09 +0300

    linked_list class

---
+++ Chapter_2/linked_list.py
@@ -0,0 +1,61 @@
+
+from random import randint
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        return str(self.value)
+    
+    
+class Linked_list:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def add_node(self, value):
+        new_node = Node(value)
+        
+        # if the old list is none, set new node as the first node
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            
+    # remove the first node that have the same value as the given node_value
+    def remove_node(self, node_value):
+        current = self.head
+        if current.value == node_value:
+            self.head = self.head.next
+        while current.next != None:
+            if current.next.value == node_value:
+                current.next = current.next.next
+                break
+            else:
+                current = current.next
+ 
+    def __str__(self):
+        if self.head != None:
+            index = self.head
+            node_store = [str(index.value)]
+            
+            while index.next != None:
+                index = index.next
+                node_store.append(str(index.value))
+            
+            return "Linked_list [{}]".format('->'.join(node_store))
+        return "Linked_list []"
+                
+    
+def random_linked_list(length, min_, max_):
+    linked_list = Linked_list()
+    for i in range(length):
+        value = randint(min_, max_)
+        linked_list.add_node(value)
+    return linked_list
